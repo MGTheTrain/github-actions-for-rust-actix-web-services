d4e6f30dd4de02579a90f12d75cecfd062174b9d
Marvin Gajek <Marvin.Gajek1@web.de>
2024-01-02 20:34:21 +0100

    introduce wrong formatting

--- hello-world-service/src/main.rs
+++ hello-world-service/src/main.rs
@@ -8,7 +8,7 @@ struct HelloWorldResponse {
 }
 
 fn get_hello_world_message() -> HelloWorldResponse {
-    HelloWorldResponse {
+        HelloWorldResponse {
         message: String::from("Hello from Rust"),
     }
 }
